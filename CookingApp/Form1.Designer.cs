4cc695f89b49d10063528eff6ccd16701efe1bdc
Baran Güngör <barangungor33@gmail.com>
2025-09-30 16:38:51 +0300

    ui tasarımı yapıldı

--- CookingApp/Form1.Designer.cs
+++ CookingApp/Form1.Designer.cs
@@ -28,12 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnYumartalariKir = new Button();
+            btnYumurtalariCirp = new Button();
+            btnTuzEkle = new Button();
+            btnOcagiAc = new Button();
+            btnTavayiIsit = new Button();
+            btnYagiDok = new Button();
+            button7 = new Button();
+            btnPisir = new Button();
+            btnServisEt = new Button();
+            label1 = new Label();
+            listBox1 = new ListBox();
+            btnStartCooking = new Button();
+            SuspendLayout();
+            // 
+            // btnYumartalariKir
+            // 
+            btnYumartalariKir.BackColor = Color.Silver;
+            btnYumartalariKir.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnYumartalariKir.Location = new Point(736, 11);
+            btnYumartalariKir.Name = "btnYumartalariKir";
+            btnYumartalariKir.Size = new Size(304, 60);
+            btnYumartalariKir.TabIndex = 0;
+            btnYumartalariKir.Text = "Yumurtaları Kır";
+            btnYumartalariKir.UseVisualStyleBackColor = false;
+            // 
+            // btnYumurtalariCirp
+            // 
+            btnYumurtalariCirp.BackColor = Color.Silver;
+            btnYumurtalariCirp.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnYumurtalariCirp.Location = new Point(736, 77);
+            btnYumurtalariCirp.Name = "btnYumurtalariCirp";
+            btnYumurtalariCirp.Size = new Size(304, 60);
+            btnYumurtalariCirp.TabIndex = 1;
+            btnYumurtalariCirp.Text = "Yumurtaları Çırp";
+            btnYumurtalariCirp.UseVisualStyleBackColor = false;
+            // 
+            // btnTuzEkle
+            // 
+            btnTuzEkle.BackColor = Color.Silver;
+            btnTuzEkle.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnTuzEkle.Location = new Point(736, 143);
+            btnTuzEkle.Name = "btnTuzEkle";
+            btnTuzEkle.Size = new Size(304, 60);
+            btnTuzEkle.TabIndex = 2;
+            btnTuzEkle.Text = "Tuz Ekle";
+            btnTuzEkle.UseVisualStyleBackColor = false;
+            // 
+            // btnOcagiAc
+            // 
+            btnOcagiAc.BackColor = Color.Silver;
+            btnOcagiAc.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnOcagiAc.Location = new Point(736, 209);
+            btnOcagiAc.Name = "btnOcagiAc";
+            btnOcagiAc.Size = new Size(304, 60);
+            btnOcagiAc.TabIndex = 3;
+            btnOcagiAc.Text = "Ocağı Aç";
+            btnOcagiAc.UseVisualStyleBackColor = false;
+            // 
+            // btnTavayiIsit
+            // 
+            btnTavayiIsit.BackColor = Color.Silver;
+            btnTavayiIsit.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnTavayiIsit.Location = new Point(736, 275);
+            btnTavayiIsit.Name = "btnTavayiIsit";
+            btnTavayiIsit.Size = new Size(304, 60);
+            btnTavayiIsit.TabIndex = 4;
+            btnTavayiIsit.Text = "Tavayı Isıt";
+            btnTavayiIsit.UseVisualStyleBackColor = false;
+            // 
+            // btnYagiDok
+            // 
+            btnYagiDok.BackColor = Color.Silver;
+            btnYagiDok.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnYagiDok.Location = new Point(736, 341);
+            btnYagiDok.Name = "btnYagiDok";
+            btnYagiDok.Size = new Size(304, 60);
+            btnYagiDok.TabIndex = 5;
+            btnYagiDok.Text = "Yağı Dök";
+            btnYagiDok.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Silver;
+            button7.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button7.Location = new Point(736, 407);
+            button7.Name = "button7";
+            button7.Size = new Size(304, 60);
+            button7.TabIndex = 6;
+            button7.Text = "Yumurtayı Ekle";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // btnPisir
+            // 
+            btnPisir.BackColor = Color.Silver;
+            btnPisir.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnPisir.Location = new Point(736, 473);
+            btnPisir.Name = "btnPisir";
+            btnPisir.Size = new Size(304, 60);
+            btnPisir.TabIndex = 7;
+            btnPisir.Text = "Pişir";
+            btnPisir.UseVisualStyleBackColor = false;
+            // 
+            // btnServisEt
+            // 
+            btnServisEt.BackColor = Color.Silver;
+            btnServisEt.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnServisEt.Location = new Point(736, 539);
+            btnServisEt.Name = "btnServisEt";
+            btnServisEt.Size = new Size(304, 60);
+            btnServisEt.TabIndex = 8;
+            btnServisEt.Text = "Servis Et";
+            btnServisEt.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Georgia", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(843, 639);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 25);
+            label1.TabIndex = 9;
+            label1.Text = "Timer";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(0, 408);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(723, 334);
+            listBox1.TabIndex = 10;
+            // 
+            // btnStartCooking
+            // 
+            btnStartCooking.Font = new Font("Georgia", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnStartCooking.Location = new Point(12, 11);
+            btnStartCooking.Name = "btnStartCooking";
+            btnStartCooking.Size = new Size(707, 61);
+            btnStartCooking.TabIndex = 11;
+            btnStartCooking.Text = "Start Cooking ";
+            btnStartCooking.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1052, 742);
+            Controls.Add(btnStartCooking);
+            Controls.Add(listBox1);
+            Controls.Add(label1);
+            Controls.Add(btnServisEt);
+            Controls.Add(btnPisir);
+            Controls.Add(button7);
+            Controls.Add(btnYagiDok);
+            Controls.Add(btnTavayiIsit);
+            Controls.Add(btnOcagiAc);
+            Controls.Add(btnTuzEkle);
+            Controls.Add(btnYumurtalariCirp);
+            Controls.Add(btnYumartalariKir);
+            Name = "Form1";
+            Text = "Task async/await";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnYumartalariKir;
+        private Button btnYumurtalariCirp;
+        private Button btnTuzEkle;
+        private Button btnOcagiAc;
+        private Button btnTavayiIsit;
+        private Button btnYagiDok;
+        private Button button7;
+        private Button btnPisir;
+        private Button btnServisEt;
+        private Label label1;
+        private ListBox listBox1;
+        private Button btnStartCooking;
     }
 }
